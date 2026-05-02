b5af6c417a590dc5f1bc4b55d1006ab80f9b4af6
Cristian Fernando Arciniega Nila <g00192002@alumnos.uteg.edu.mx>
2026-05-02 08:32:01 +0000

    Fix HiddenCloud startup when dist is missing

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 require('dotenv/config');
-require('./dist/index.js');
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const distEntry = path.join(__dirname, 'dist', 'index.js');
+
+if (fs.existsSync(distEntry)) {
+	require(distEntry);
+} else {
+	try {
+		require('ts-node/register/transpile-only');
+		require('./index.ts');
+	} catch (error) {
+		console.error('No se pudo iniciar el bot: dist/index.js no existe y ts-node no esta disponible.');
+		console.error(error);
+		process.exit(1);
+	}
+}
